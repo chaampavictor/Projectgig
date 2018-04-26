54c25fd88f72352ede3f311e3651f8c321fe72e3
chaampavictor <victorchaampa98@gmail.com>
2018-04-26 12:19:55 +0200

    updated the packages, worked in the ui/ux

---
+++ client/userdisplay/Username.jsx
@@ -0,0 +1,31 @@
+// import React, {Component} from 'react';
+// import {Meteor} from 'meteor/meteor';
+//
+// class FileInput extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+//   handleSubmit(event) {
+//     event.preventDefault();
+//     alert(`Selected file - ${this.fileInput.files[0].name}`);
+//   }
+//
+//   render() {
+//     return (
+//       <form onSubmit={this.handleSubmit}>
+//         <label>
+//           Upload file:
+//           <input type="file" ref={input => {
+//             this.fileInput = input;
+//           }}/>
+//         </label>
+//         <br/>
+//         <button type="submit">Submit</button>
+//       </form>
+//     );
+//   }
+// }
+//
+// ReactDOM.render(
+//   <FileInput/>, document.getElementById('root'));

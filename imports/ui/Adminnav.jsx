9e341395426ee3a8980f2e2774f2f4bb01ad86c6
chaampavictor <victorchaampa98@gmail.com>
2018-10-20 15:13:29 +0200

    modified adminnav

--- imports/ui/Adminnav.jsx
+++ imports/ui/Adminnav.jsx
@@ -9,7 +9,7 @@ export default class Adminnav extends Component {
           <a href="/dashboard" id="trying">Nidavel Admin</a>
             <ul id="nav-mobile" className="right">
               <li><a href="/" id="trying">Home</a></li>
-              <li><a href="/property" id="trying">Add Property</a></li>
+              <li><a href="/profile" id="trying">Profile</a></li>
               <li><a href="/listedproperty" id="trying">Listed Property</a></li>
               <li><a href="/dashuser" id="trying">Users</a></li>
             </ul>
